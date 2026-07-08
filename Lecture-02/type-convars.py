4a25b80ed8f4fb351e382c6c7e2d6cd9d4ff664c
piyathap2307@gmail.com <piyathap2307@gmail.com>
2026-07-08 09:24:26 +0700

    Add operator and type conversion scripts in Lecture-02

---
+++ Lecture-02/type-convars.py
@@ -0,0 +1,10 @@
+age = 25
+height = float(age)
+print("Height as float:", height)
+height = 5.9
+age = int(height)
+print("Age as int:", age)
+
+num_str = "123"
+num_int = int(num_str)
+print("String to int:", num_int)
