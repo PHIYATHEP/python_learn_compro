47fc6d5acce1f1ccb85fd9ca8973eb403818186b
piyathap2307@gmail.com <piyathap2307@gmail.com>
2026-07-08 08:08:43 +0700

    Add initial Python scripts for learning exercises

---
+++ Hello.py
@@ -0,0 +1,7 @@
+print("Hello, World!");
+print("Information Network Engineering");
+print("Hello Phiyathep you will be a great Programmer!");
+print("I'am Phiyathep");
+print('He ask me "Who are you?"');
+print("I'm good engineer, \"I will be a great programmer!\"");# \สามารถฆ่า " ได้
+print("/\\");
---
+++ strNum.py
@@ -0,0 +1,8 @@
+print('My age is',25,'I have',3500.50,'Bth');
+print('My age is %d I have %d Bth' % (25, 3500.50));
+print('My age is '+str(25)+' I have '+str(3500.50)+' Bth');
+print('My age is {0} I have {1:.2f} Bth'.format(25, 3500.50));
+num2 = 3500.50;
+print(f"My age is {55} I have {num2:.2f} Bth");
+print(1, 1, 2, 3,5, 8, 13, 21, 34, 55);
+print("%d %d %d %d %d %d %d %d %d %d" %(1, 1, 2, 3, 5, 8, 13, 21, 34, 55));
