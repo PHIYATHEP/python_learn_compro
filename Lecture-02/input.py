750301eec32f93d32647af383243af4a5f97b6f8
piyathap2307@gmail.com <piyathap2307@gmail.com>
2026-07-08 10:12:43 +0700

    Add scripts for user input and information display in Lecture-02

---
+++ Lecture-02/input.py
@@ -0,0 +1,4 @@
+fname=input("Enter your first name: ")
+lname=input("Enter your last name: ")
+result=fname+" "+lname
+print("Your full name is:",result) 
---
+++ Lecture-02/tset_input_num.py
@@ -0,0 +1,9 @@
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+income=int(input("Enter your income: "))
+
+print("The information you entered is:")
+print("Your name is:",name)
+print("Your age is:",age)
+print("Your income is:",format(income,'.2f'))
+
