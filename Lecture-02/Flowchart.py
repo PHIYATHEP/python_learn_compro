356f5718d5c61fb60e51b0a70d5bbe25d03c303b
piyathap2307@gmail.com <piyathap2307@gmail.com>
2026-07-08 08:45:07 +0700

    Add Flowchart script for rectangle area calculation in Lecture-02

---
+++ Lecture-02/Flowchart.py
@@ -0,0 +1,11 @@
+# รับค่าความกว้าง
+width = float(input("Enter the width of Rectangle: "))
+
+# รับค่าความสูง
+height = float(input("Enter the height of Rectangle: "))
+
+# คำนวณพื้นที่
+area = width * height
+
+# แสดงผลลัพธ์
+print("The area of Rectangle is:", area)
